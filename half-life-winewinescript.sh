48dec4ed6d12c133f1fb9174d980427d3120b040
Jonah Hirsch <crazydog115@gmail.com>
2025-01-31 23:50:00 -0800

    Add half-life (wine)

---
+++ half-life-winewinescript.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+SCRIPT_NAME=$(echo \"$0\" | xargs readlink -f)
+SCRIPTDIR=$(dirname "$SCRIPT_NAME")
+
+exec 6>display.log
+/usr/bin/Xvfb -displayfd 6 -nolisten tcp -nolisten unix &
+XVFB_PID=$!
+while [[ ! -s display.log ]]; do
+  sleep 1
+done
+read -r DPY_NUM <display.log
+rm display.log
+
+export WINEPREFIX="$SCRIPTDIR/half-life-wine/.wine"
+export WINEDLLOVERRIDES="mscoree,mshtml="
+export WINEARCH=win64
+export WINEDEBUG=fixme-all
+export DISPLAY=:$DPY_NUM
+
+exec 6>&-
+kill $XVFB_PID
+
+exit 0
